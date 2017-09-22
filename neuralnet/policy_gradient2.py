ef595676e081eb13d0ee40a55637d84dd6576757
Chao <cgao3@ualberta.ca>
2017-09-22 10:55:05 -0600

    test v3

--- neuralnet/policy_gradient2.py
+++ neuralnet/policy_gradient2.py
@@ -132,6 +132,7 @@ class PolicyGradient(object):
             else:
                 logits_score = otherSess.run(otherlogits, feed_dict={otherxnode: self.input_tensor})
             if turn == first_player:
+                logits_score = np.squeeze(logits_score)
                 best_action=-1
                 largest_score=0
                 for action in empty_points:
@@ -237,6 +238,9 @@ class PolicyGradient(object):
             self.sess.run(optimizer, feed_dict={self.cnn.x_node_dict[self.boardsize]: paUtil.batch_positions,
                                            self.cnn.y_star: paUtil.batch_labels, rewards_node: rewards})
             ite += 1
+            if ite % 10 == 0:
+                self.saver.save(self.sess, os.path.join(output_dir, outputname), global_step=ite)
+
             if is_alphago_like:
                 l2 = [f for f in os.listdir(output_dir) if f.endswith(".meta")]
                 if len(l2) == 0:
@@ -250,11 +254,10 @@ class PolicyGradient(object):
                 self.aux_saver.restore(self.other_sess, selected_model)
                 self.input_tensor.fill(0)
                 print(self.other_sess.run(self.aux_logits, feed_dict={self.cnn2.x_node_dict[self.boardsize]: self.input_tensor}))
-            if ite % 10 == 0:
-                self.saver.save(self.sess, os.path.join(output_dir, outputname), global_step=ite)
 
         self.saver.save(self.sess, os.path.join(output_dir, outputname), global_step=ite)
         self.sess.close()
+        self.other_sess.close()
         print('Done PG training')
 
     def policygradient_adversarial_v1(self, output_dir, is_alphago_like=False):
@@ -310,6 +313,9 @@ class PolicyGradient(object):
                                            self.cnn.y_star: paUtil.batch_labels, rewards_node: rewards})
 
             ite += 1
+            if ite % 10 == 0:
+                self.saver.save(self.sess, os.path.join(output_dir, outputname), global_step=ite)
+
             if is_alphago_like:
                 l2 = [f for f in os.listdir(output_dir) if f.endswith(".meta")]
                 if len(l2) == 0:
@@ -324,11 +330,9 @@ class PolicyGradient(object):
                 self.input_tensor.fill(0)
                 print(self.other_sess.run(self.aux_logits, feed_dict={self.cnn2.x_node_dict[self.boardsize]: self.input_tensor}))
 
-            if ite % 10 == 0:
-                self.saver.save(self.sess, os.path.join(output_dir, outputname), global_step=ite)
-
         self.saver.save(self.sess, os.path.join(output_dir, outputname), global_step=ite)
         self.sess.close()
+        self.other_sess.close()
         print('Done adversarial v1 PG training with min return')
 
     '''
@@ -351,11 +355,11 @@ class PolicyGradient(object):
         paUtil = OnlinePositionActionUtil(batch_size=batch_size, boardsize=self.boardsize)
 
         ite = 0
-        outputname = 'adversarial_pg_v1.model' + repr(self.boardsize) + 'x' + repr(self.boardsize)
+        outputname = 'adversarial_pg_v2.model' + repr(self.boardsize) + 'x' + repr(self.boardsize)
         if is_alphago_like:
-            outputname = 'adverv1_pg_alphagolike.model' + repr(self.boardsize) + 'x' + repr(self.boardsize)
+            outputname = 'adverv2_pg_alphagolike.model' + repr(self.boardsize) + 'x' + repr(self.boardsize)
         while ite < max_iterations:
-            print('apg v1 iteration ', ite)
+            print('apg v2 iteration ', ite)
             if is_alphago_like:
                 intgamelist, resultlist = self.playbatchgame(batch_size, self.this_logits, self.cnn.x_node_dict[self.boardsize],
                                                              self.aux_logits, self.cnn2.x_node_dict[self.boardsize], self.sess, self.other_sess)
@@ -388,7 +392,8 @@ class PolicyGradient(object):
             self.sess.run(optimizer, feed_dict={self.cnn.x_node_dict[self.boardsize]: paUtil.batch_positions,
                                                 self.cnn.y_star: paUtil.batch_labels, rewards_node: rewards})
             ite += 1
-
+            if ite % 10 == 0:
+                self.saver.save(self.sess, os.path.join(output_dir, outputname), global_step=ite)
             if is_alphago_like:
                 l2 = [f for f in os.listdir(output_dir) if f.endswith(".meta")]
                 if len(l2) == 0:
@@ -401,19 +406,106 @@ class PolicyGradient(object):
                 self.input_tensor.fill(0)
                 print(self.other_sess.run(self.aux_logits, feed_dict={self.cnn2.x_node_dict[self.boardsize]: self.input_tensor}))
 
+        self.saver.save(self.sess, os.path.join(output_dir, outputname), global_step=ite)
+        self.sess.close()
+        self.other_sess.close()
+        print('Done adversarial v2 PG training with min return')
+
+    def policygradient_adversarialv3(self, output_dir, is_alphago_like=False):
+        batch_size = self.hpr['batch_size']
+        max_iterations = self.hpr['max_iteration']
+        learning_rate = self.hpr['step_size']
+        topk = self.hpr['topk']
+        '''
+        PG use the same architecture except that the loss function has a Reward value!
+        '''
+        with self.g.as_default():
+            crossentropy = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=self.cnn.y_star, logits=self.this_logits)
+            rewards_node = tf.placeholder(dtype=tf.float32, shape=(None,), name='reward_node')
+            loss = tf.reduce_mean(tf.multiply(rewards_node, crossentropy))
+            optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate / batch_size).minimize(loss)
+        rewards = np.ndarray(shape=(batch_size,), dtype=np.float32)
+        paUtil = OnlinePositionActionUtil(batch_size=batch_size, boardsize=self.boardsize)
+
+        ite = 0
+        outputname = 'adversarial_pg_v3.model' + repr(self.boardsize) + 'x' + repr(self.boardsize)
+        if is_alphago_like:
+            outputname = 'adverv3_pg_alphagolike.model' + repr(self.boardsize) + 'x' + repr(self.boardsize)
+        while ite < max_iterations:
+            print('apg v3 iteration ', ite)
+            if is_alphago_like:
+                intgamelist, resultlist = self.playbatchgame(batch_size, self.this_logits, self.cnn.x_node_dict[self.boardsize],
+                                                             self.aux_logits, self.cnn2.x_node_dict[self.boardsize], self.sess, self.other_sess)
+            else:
+                intgamelist, resultlist = self.playbatchgame(batch_size, self.this_logits,
+                                                             self.cnn.x_node_dict[self.boardsize], self.this_logits,
+                                                             self.cnn.x_node_dict[self.boardsize], self.sess, self.sess)
+            positionactionlist = []
+            for i in range(batch_size):
+                intgame = intgamelist[i]
+                length = len(intgame)
+                j = np.random.randint(2, length - 1)
+                s_a = intgame[:j]
+                positionactionlist.append(s_a)
+                r1 = -resultlist[i] if len(s_a) % 2 == 0 else resultlist[i]
+                minR = r1
+                sprime=intgame[:j]
+
+                self.input_tensor.fill(0)
+                self.input_tensor_builder.set_position_tensors_in_batch(self.input_tensor, 0, sprime)
+                logits_score = self.sess.run(self.this_logits, feed_dict={self.cnn.x_node_dict[self.boardsize]: self.input_tensor})
+                logits_score = np.squeeze(logits_score)
+                top_points = np.argpartition(-logits_score, kth=topk)[:topk]
+                top_points = top_points.tolist()
+                for pp in top_points:
+                    if pp in sprime:
+                        top_points.remove(pp)
+
+                for top_next_action in top_points:
+                    sprime.append(top_next_action)
+                    if is_alphago_like:
+                        ret_gamestate, reward = self.playonegame(sprime, self.this_logits, self.cnn.x_node_dict[self.boardsize],
+                                                                             self.aux_logits,
+                                                                             self.cnn2.x_node_dict[self.boardsize], self.sess, self.other_sess)
+                    else:
+                        ret_gamestate, reward = self.playonegame(sprime, self.this_logits, self.cnn.x_node_dict[self.boardsize],
+                                                                             self.this_logits,
+                                                                             self.cnn.x_node_dict[self.boardsize], self.sess, self.sess)
+                    if len(s_a) % 2 == 0:
+                        reward = -reward
+                    minR = min(minR, reward)
+                    sprime.remove(top_next_action)
+
+                rewards[i] = minR
+            paUtil.prepare_next_batch(positionactionlist)
+            self.sess.run(optimizer, feed_dict={self.cnn.x_node_dict[self.boardsize]: paUtil.batch_positions,
+                                                self.cnn.y_star: paUtil.batch_labels, rewards_node: rewards})
+            ite += 1
             if ite % 10 == 0:
                 self.saver.save(self.sess, os.path.join(output_dir, outputname), global_step=ite)
+            if is_alphago_like:
+                l2 = [f for f in os.listdir(output_dir) if f.endswith(".meta")]
+                if len(l2) == 0:
+                    continue
+                selected_model = np.random.choice(l2)
+                selected_model = selected_model[0:-len('.meta')]
+                selected_model = os.path.join(output_dir, selected_model)
+                print('selected model:', selected_model)
+                self.aux_saver.restore(self.other_sess, selected_model)
+                self.input_tensor.fill(0)
+                print(self.other_sess.run(self.aux_logits, feed_dict={self.cnn2.x_node_dict[self.boardsize]: self.input_tensor}))
 
         self.saver.save(self.sess, os.path.join(output_dir, outputname), global_step=ite)
         self.sess.close()
-        print('Done adversarial v2 PG training with min return')
+        self.other_sess.close()
+
 
 
 if __name__ == "__main__":
     import argparse
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--max_train_step', type=int, default=200, help='maximum training steps or iterations')
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--max_train_step', type=int, default=500, help='maximum training steps or iterations')
     parser.add_argument('--batch_train_size', type=int, default=243, help='batch size, default boardsize*boardsize*3')
     parser.add_argument('--output_dir', type=str, default='/tmp/saved_checkpoint/', help='where to save logs')
 
@@ -422,11 +514,13 @@ if __name__ == "__main__":
     parser.add_argument('--boardsize', type=int, default=9, help='default 9')
     parser.add_argument('--n_hidden_layer', type=int, default=6, help='default 6')
 
-    parser.add_argument('--topk', type=int, default=3, help='default 1')
+    parser.add_argument('--topk', type=int, default=3, help='default 3')
 
     parser.add_argument('--alphago_like', action='store_true', default=False, help='binary value, default False')
     parser.add_argument('--step_size', type=float, default=0.01, help='policy gradient step_size (learning rate)')
     parser.add_argument('--adversarial', action='store_true', default=False, help='binary value, default False')
+    parser.add_argument('--adversarialv2', action='store_true', default=False, help='using deterministic playout?')
+    parser.add_argument('--adversarialv3', action='store_true', default=False, help='one step lookahead then playout?')
     args = parser.parse_args()
 
     if not os.path.isdir(args.output_dir):
@@ -444,7 +538,7 @@ if __name__ == "__main__":
 
     pg=PolicyGradient(args.boardsize, n_hidden_layer=args.n_hidden_layer,
                    previous_checkpoint=args.previous_checkpoint, hyperparameter=hyperparameter)
-    if not args.alphago_like and not args.adversarial:
+    if not args.alphago_like and not args.adversarial and not args.adversarialv2 and not args.adversarialv3:
        print('Doing REINFORCE policy gradient')
        pg.policygradient_vanilla(output_dir=args.output_dir, is_alphago_like=False)
        exit(0)
@@ -455,7 +549,21 @@ if __name__ == "__main__":
         exit(0)
 
     if args.adversarial:
-        hyperparameter['topk']=3
-        pg.policygradient_adversarial_v1(output_dir=args.output_dir)
         print('Doing adversarial policy gradient')
+        pg.policygradient_adversarial_v1(output_dir=args.output_dir)
         exit(0)
+
+    if args.adversarialv2:
+        print('Doing adversarial policy gradient v2')
+        pg.policygradient_adversarial_v2(output_dir=args.output_dir)
+
+        exit(0)
+
+    if args.adversarialv3:
+        print('Doing adversarial policy gradient v3')
+        pg.policygradient_adversarialv3(output_dir=args.output_dir)
+
+        exit(0)
+
+
+
